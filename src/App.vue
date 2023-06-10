12d57b618ebe8b03331b9ae3b26fa4f7c68fcf39
Cedar <1752440588@qq.com>
2023-06-10 17:35:13 +0800

    vue购物车案例

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-container">
+    <!-- header头部区域 -->
+    <Header title="购物车案例"></Header>
+    <h1>App 根组件</h1>
+    <!-- 循环渲染每一个商品的信息 -->
+    <Goods v-for="item in list" :key="item.id" :title="item.goods_name" :pic="item.goods_img" :state="item.goods_state" :id="item.id" :count="item.goods_count" @state-change="getNewState"></Goods>
+    <!-- Footer区域 -->
+    <!-- 编码规范：先指令，再绑定，再事件 -->
+    <Footer :isfull="fullState" :amount="amt" :all="total" @full-change="getFullState"></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//导入 axios 请求库
+import axios from 'axios';
+
+import Header from "./components/Header/Header.vue";
+import Goods from "./components/Goods/Goods.vue";
+import Footer from "./components/Footer/Footer.vue"
+
+import bus from './components/eventBus.js'
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      // 用来存储购物车的列表数据，默认为空数组
+      list:[]
+
+    }
+  },
+  //计算属性
+  computed: {
+    //动态计算出全选的状态是true还是false
+    fullState() {
+      return this.list.every(item => item.goods_state)
+    },
+    //已勾选商品的总价格
+    amt() {
+      // 1.先 filter过滤
+      // 2. 再reduce累加
+      this.list.filter(item => item.goods_state).reduce((total, item) => {
+        total += item.goods_price * item.goods_count
+      }, 0)
+    }
+  },
+
+  created() {
+    //调用请求数据的方法
+    this.initCartList()
+    bus.$on('share', val => {
+      this.list.some(item => {
+        if(item.id === val.id) {
+          item.goods_count = val.value
+          return true
+        }
+      })
+    })
+  },
+  methods: {
+    //封装请求列表数据的方法
+    async initCartList() {
+      // 调用 axios 的get方法，请求列表数据
+      const {data: res} = await axios.get('https://www.escook.cn/api/cart')
+      //只要请求回来的数据，在页面渲染期间要用到，则必须转存到data中
+      if(res.status == 200) {
+        this.list = res.list
+      }
+    },
+    //接收子组件传递过来的数据
+    //e的格式为{ id, value}
+    getNewState(e) {
+      console.log('父组件接收到了数据');
+      if(item.id === e.id) {
+        item.goods_state = e.value
+      }
+    },
+    //接收 Footer 子组件传递过来的全选按钮的状态
+    getFullState(val) {
+      console.log('在App中拿到了全选的状态');
+      this.list.forEach(item => (item.goods_state = val))
+    }
+  },
   components: {
-    HelloWorld
+    Header,
+    Goods,
+    Footer
   }
+  
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+.app_container {
+  padding-top: 45px;
+  padding-bottom: 50px;
 }
 </style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header-container">{{ title }}</div>
+</template>
+
+<script>
+export default {
+props: {
+    //声明title自定义属性，允许使用者自定义标题的内容
+    default: '',
+    type: String
+}
+}
+</script>
+
+<style lang="less" scoped>
+.header-container {
+    font-size: 12px;
+    height: 45px;
+    width: 100%;
+    background-color: lightblue;
+}
+</style>
